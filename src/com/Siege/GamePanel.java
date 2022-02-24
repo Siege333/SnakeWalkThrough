ab127f89160f95ead3031d53e1cfd8225133e323
Siege333 <celiacreaghan@gmail.com>
2022-02-24 13:02:24 +0000

    added border collisions and apple collisions

--- src/com/Siege/GamePanel.java
+++ src/com/Siege/GamePanel.java
@@ -56,7 +56,7 @@ public class GamePanel extends JPanel implements ActionListener{
 		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 		
 		for(int i = 0; i< bodyParts; i++) {
-			if(i ==0) {
+			if(i == 0) {
 				g.setColor(Color.green);
 				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 			}
@@ -96,10 +96,41 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	
 	public void checkApple() {
-		
+		if((x[0] == appleX) && (y[0] == appleY)) {
+			bodyParts++;
+			applesEaten++;
+			newApple();
+			
+		}
 	}
 	
 	public void checkCollisions() {
+		//this checks if head collides with body
+		for (int i = bodyParts; i > 0; i--) {
+			if((x[0]) == x[i] && (y[0] == y[i])) {
+				running = false;
+			}
+		}
+		//check if head touches left border
+		if(x[0] < 0) {
+			running = false;
+		}
+		//check if head touches right border
+		if(x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		//check if head touches top border
+		if(y[0] < 0) {
+			running = false;
+		}
+		//check if head touches bottom border
+		if(y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+		
+		if(!running) {
+			timer.stop();
+		}
 		
 	}
 	
@@ -108,14 +139,40 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
+		if (running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 	}
 
 	public class MyKeyAdapter extends KeyAdapter{
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U') {
+					direction = 'D';
+				}
+				break;
+			}
 		}
 	}
 	
